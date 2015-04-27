2212009211f8ea039ce321cd33de36a4d12dca99
Tom Crayford <tcrayford@googlemail.com>
2015-04-27 21:36:22 +0800

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bolth "0.1.0-SNAPSHOT"
+(defproject bolth "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
