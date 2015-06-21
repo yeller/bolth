6ad12031396c3837055c9ff69c76763aac3571d2
Tom Crayford <tcrayford@googlemail.com>
2015-06-21 18:18:57 +0100

    bump description/url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bolth "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a better test runner for clojure.test"
+  :url "https://github.com/yeller/bolth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
